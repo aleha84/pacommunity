4d0cebf8fa589a82f51446421fb2786297f517d4
Starykh Aleksey <astarikh@mail.ru>
2021-03-08 12:13:26 +0300

    site\data update

--- common/scripts.js
+++ common/scripts.js
@@ -269,7 +269,7 @@ function parseParams(str) {
         if(created)
             created.remove();
 
-        let v = '6.6.17';
+        let v = '6.6.18';
         if(!search) {
             let searchResponse = await fetch(this.params.rootFolderPath + 'common/html/search.html?v=' + v);
             let searchHtml = await searchResponse.text();
